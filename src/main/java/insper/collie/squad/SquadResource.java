1a1275ded29ab14ba3ca84b233b9e02b69009200
liviatanaka <liviatanaka19@gmail.com>
2024-04-05 17:06:06 -0300

    read, update and delete

--- src/main/java/insper/collie/squad/SquadResource.java
+++ src/main/java/insper/collie/squad/SquadResource.java
@@ -1,6 +1,8 @@
 package insper.collie.squad;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -31,6 +33,50 @@ public class SquadResource implements SquadController {
             .body(SquadParser.to(squad));
     }
 
+    @Override
+    public ResponseEntity<SquadInfo> getSquad(String id){
+
+        Squad squad = squadService.getSquad(id);
+        if (squad == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(SquadParser.to(squad));
+    }
+
+    @Override
+    public ResponseEntity<List<SquadInfo>> getAllSquads(){
+
+        List<Squad> squads = squadService.getAllSquads();
+        if (squads.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(
+            squads.stream()
+                .map(SquadParser::to)
+                .collect(Collectors.toList())
+        );
+    }
+
+    @Override
+    public ResponseEntity<SquadInfo> updateSquad(String id, SquadInfo in){
+
+        Squad Squad = SquadParser.to(in);
+        Squad = squadService.update(id, Squad);
+        if (Squad == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(SquadParser.to(Squad));
+    }
+
+    @Override
+    public ResponseEntity<SquadInfo> deleteSquad(String id){
+
+        String r = squadService.delete(id);
+        if (r == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().build();
+    }
 
     
 }
--- src/main/java/insper/collie/squad/SquadService.java
+++ src/main/java/insper/collie/squad/SquadService.java
@@ -7,6 +7,12 @@ import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import lombok.NonNull;
 
@@ -16,8 +22,59 @@ public class SquadService {
     @Autowired
     private SquadRepository squadRepository;
 
+    
+
     public Squad create(Squad in) {
+
         return squadRepository.save(new SquadModel(in)).to();
+    }
+
+    @Transactional(readOnly = true)
+    public Squad getSquad(String id) {
+        return squadRepository.findById(id).map(SquadModel::to).orElse(null);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Squad> getAllSquads() {
+        List<Squad> squads = new ArrayList<Squad>();
+
+        for (SquadModel c : squadRepository.findAll()){
+            squads.add(c.to());
+        };
+
+        return squads;
+    }
+
+    @Transactional
+    public Squad update(String id, Squad in) {
+        SquadModel c = squadRepository.findById(id).orElse(null);
+        if (c == null){
+            return null;
+        }
+
+        SquadModel squad = c;
+
+        if (in.name() != null) {
+            squad.name(in.name());
+        }
+        if(in.description() != null){
+            squad.description(in.description());
+        }
+        if(in.company_id != null){
+            squad.company_id(in.company_id());
+        }
+
+        return squadRepository.save(squad).to();
+    }
+
+    @Transactional
+    public String delete(String id) {
+        SquadModel c = squadRepository.findById(id).orElse(null);
+        if (c == null){
+            return null;
+        }
+        squadRepository.deleteById(id);
+        return "ok";
     }
     
 }
